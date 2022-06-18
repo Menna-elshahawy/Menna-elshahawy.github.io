0892a952e4442b904f370b8effb383a96a93873f
Menna-elshahawy <menna.elshahawy95@gmail.com>
2022-06-18 20:42:17 +0200

    Update social links in footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -17,19 +17,19 @@ class Footer extends Component {
 		    <div className="mt-5 pt-5 pb-5 footer centered">
 		    	<h1 className="footer_name">Menna Elshahawy</h1>
 		    	<div>
-						<a href="https://www.facebook.com/learnbuildteach/"
+						<a href="https://www.facebook.com/menna.elshahawy95/" target="_blank"
 	                className="facebook social">
 	                <FontAwesomeIcon icon={faFacebook} size="2x" />
 	              </a>
-	              <a href="https://www.youtube.com/c/jamesqquick"
+	              <a href="https://www.youtube.com/channel/UC-0njRKg2ODD766T7LP6tdQ" target="_blank"
 	                className="youtube social">
 	                <FontAwesomeIcon icon={faYoutube} size="2x" />
 	              </a>
-	            <a href="https://www.instagram.com/learnbuildteach"
+	            <a href="https://www.linkedin.com/in/menna-elshahawy/" target="_blank"
 	                className="facebook social">
 	                <FontAwesomeIcon icon={faLinkedin} size="2x" />
 	            </a>
-	              <a href="https://www.instagram.com/learnbuildteach"
+	              <a href="https://www.instagram.com/menna_elshahawy/" target="_blank"
 	                className="instagram social">
 	                <FontAwesomeIcon icon={faInstagram} size="2x" />
 	            </a>
